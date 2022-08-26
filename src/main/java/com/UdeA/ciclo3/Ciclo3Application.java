b66d4989973c88ee566f28ad9d69445cfa9a9b09
Juliana Caicedo <julianacaicedo55@gmail.com>
2022-08-26 16:03:44 -0500

    Clase empresa

--- src/main/java/com/UdeA/ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/ciclo3/Ciclo3Application.java
@@ -1,10 +1,13 @@
 package com.UdeA.ciclo3;
 
+import com.UdeA.ciclo3.modelos.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.bind.SchemaOutputResolver;
 
 @RestController
 @SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
@@ -14,8 +17,19 @@ public class Ciclo3Application {
 	public String hello(){
 		return "Hola ciclo 3 ... Salderms vivos de esto";
 	}
+
+	@GetMapping("/test")
+	public String test(){
+		Empresa empresa = new Empresa("FRUTI DULCES", "CALLE 5 #95 -24", "7895625", "800221314-5");
+		empresa.setNombre("PRODUCTOS FRUTI DULCES");
+		return empresa.getNombre();
+	}
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
+
 	}
 
 }
